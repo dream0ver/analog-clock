2c3565666a628ec56ef4c6b36133fecae219a0b2
dream0ver <krishnachaithanya826@gmail.com>
2023-03-26 18:18:39 +0530

    micro adjustments in clock hands bug fix

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ const set_second_hand = seconds => {
   const angle = 6 * seconds
   second_hand.style.transform = `rotate(${angle + 90}deg)`
 }
-const set_minute_hand = minutes => {
+const set_minute_hand = (minutes, seconds) => {
   const minute_hand = document.getElementById("minute_hand")
-  const angle = 6 * minutes
+  const angle = 6 * minutes + Math.floor(seconds / 10)
   minute_hand.style.transform = `rotate(${angle + 90}deg)`
 }
-const set_hour_hand = hours => {
+const set_hour_hand = (hours, minutes) => {
   const hour_hand = document.getElementById("hour_hand")
-  const angle = 30 * hours
+  const angle = 30 * hours + Math.floor(minutes / 2)
   hour_hand.style.transform = `rotate(${angle + 90}deg)`
 }
 const updateTime = () => {
@@ -31,8 +31,8 @@ const updateTime = () => {
   const minutes = date.getMinutes()
   const hours = date.getHours()
   set_second_hand(seconds)
-  set_minute_hand(minutes)
-  set_hour_hand(hours)
+  set_minute_hand(minutes, seconds)
+  set_hour_hand(hours, minutes)
 }
 renderClock()
 updateTime()
